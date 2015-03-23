4c8b0ceb26078b1da64882077765ee5becab18c1
root <root@kame204.ed.yama.tus.ac.jp>
2015-03-23 21:45:39 +0900

    update version

--- version.php
+++ version.php
@@ -1,7 +1,7 @@
 <?php
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2015030200;           // The current plugin version (Date: YYYYMMDDXX)
+$plugin->version   = 2015032300;           // The current plugin version (Date: YYYYMMDDXX)
 $plugin->requires  = 2013110500;           // Requires this Moodle version
 $plugin->component = 'filter_quiz_chart';  // Full name of the plugin (used for diagnostics)
 $plugin->maturity = MATURITY_BETA;
